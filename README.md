7dda70771a1b675841392e27c2e49f581108835f
unknown <shanyaverma2349@gmail.com>
2022-11-03 15:08:59 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # web_dev
 # web_dev
 # web_dev
+# web_dev
---
+++ app.js
@@ -0,0 +1,8 @@
+let val;
+
+val = "vineet" 
+
+document.getElementById('task-title').style.color = "red" ; 
+document.getElementById("task-title").innerText = "Johnny Bravo"; 
+
+console.log(val); 
